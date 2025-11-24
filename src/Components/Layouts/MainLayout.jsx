e03fcf5de7761cb9f42151a6d402c2a7e838b669
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-11-24 12:59:04 +0600

    fixed problem

--- src/Components/Layouts/MainLayout.jsx
+++ src/Components/Layouts/MainLayout.jsx
@@ -7,7 +7,7 @@ import Footer from "./Footer";
 const MainLayout = () => {
   return (
     <>
-      <div className="flex flex-col h-screen">
+      <div className="flex flex-col min-h-screen">
         <Navbar />
         <div className="flex-1">
           <Outlet />
